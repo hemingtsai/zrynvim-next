79fc492e875d1ab4642fb4133262274cfd9111bf
hemingtsai <hemingtsai@hotmail.com>
2026-08-26 22:04:37 +0800

    fix(mason): update repo to mason-org/mason.nvim, remove deprecated ensure_installed
    - Repo migrated: williamboman/mason.nvim → mason-org/mason.nvim
    - ensure_installed removed (no longer supported; use :Mason to install manually)
    - Fixed file path comment to match actual location

--- lua/code/plugins/mason.lua
+++ lua/code/plugins/mason.lua
@@ -1,16 +1,11 @@
--- lua/tools/plugins/mason.lua
+-- lua/code/plugins/mason.lua
+-- LSP 工具安装器：运行 :Mason 手动安装服务器
 return {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     cmd = "Mason",
     build = ":MasonUpdate",
     config = function()
         require("mason").setup({
-            ensure_installed = {
-                "typescript-language-server",
-                "tailwindcss-language-server",
-                "lua-language-server",
-                -- 你可以添加其他工具：stylua, prettier, clang-format 等
-            },
             PATH = "prepend",
         })
     end,
